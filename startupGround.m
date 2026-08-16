dc64472800f84c8f2fda4dcd836c90a8e6bba779
Muhozaplt <pilotemuhoza@outlook.com>
2026-08-16 14:35:58 +1000

    fresh files

---
+++ startupGround.m
@@ -0,0 +1,24 @@
+%% close previously open model
+close_system('sl_groundvehicleDynamics',0);
+ 
+
+
+
+%% add toolboxes to path
+homedir = pwd; 
+addpath( genpath(strcat(homedir,[filesep,'toolboxes'])));
+
+cd('toolboxes/MRTB');
+startMobileRoboticsSimulationToolbox;
+
+cd(homedir);
+
+%% open current model
+open_system('sl_groundvehicleDynamics'); % ground robot model
+
+cd(homedir);
+
+
+
+
+
